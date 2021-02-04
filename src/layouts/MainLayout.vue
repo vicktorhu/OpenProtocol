37dec103f8f535b8094cef58a86b65c1dcc73de6
vicktorhu <vicktor.hutama@gmail.com>
2021-02-04 13:27:18 +0700

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Todo Vue 3 Typescript Demo App
+        </q-toolbar-title>
+
+        <!-- <div>Quasar v{{ $q.version }}</div> -->
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      content-class="bg-grey-10 border-grey-10"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-1"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Home',
+    // caption: 'Home Screen',
+    icon: 'home',
+    link: '/#/'
+  },
+  {
+    title: 'Todo list',
+    // caption: 'Todo List App',
+    icon: 'list',
+    link: '/#/todolist'
+  },
+];
+
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'MainLayout',
+  components: { EssentialLink },
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const essentialLinks = ref(linksData);
+
+    return {leftDrawerOpen, essentialLinks}
+  }
+});
+</script>
